ebd71cb79e2c032946ebc86cdbebc013ec572ecd
messense <messense@icloud.com>
2015-03-27 16:59:12 +0800

    Add RetryMiddleware with random proxy support

---
+++ douappbook/middlewares.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+import re
+import random
+import base64
+import urllib
+try:
+    import urllib.parse as urlparse
+except ImportError:
+    import urlparse
+
+from scrapy import log
+from scrapy.contrib.downloadermiddleware.retry import RetryMiddleware as _RetryMiddleware
+
+
+class RetryMiddleware(_RetryMiddleware):
+
+    def __init__(self, settings):
+        super(RetryMiddleware, self).__init__(settings)
+        self.proxies = {}
+        self.proxy_list = settings.get('PROXY_LIST')
+        if not self.proxy_list:
+            return
+
+        try:
+            with open(self.proxy_list) as f:
+                lines = f.readlines()
+        except (IOError, EOFError):
+            return
+
+        for line in lines:
+            parts = re.match('(\w+://)(\w+:\w+@)?(.+)', line)
+
+            # Cut trailing @
+            if parts.group(2):
+                user_pass = parts.group(2)[:-1]
+            else:
+                user_pass = ''
+
+            self.proxies[parts.group(1) + parts.group(3)] = user_pass
+
+    def _retry(self, request, reason, spider):
+        retries = request.meta.get('retry_times', 0) + 1
+        last_proxy = request.meta['proxy']
+        if last_proxy and self.proxies:
+            self.proxies.pop(last_proxy, None)
+            log.msg('Removing failed proxy <%s>, %d proxies left' % (
+                last_proxy, len(self.proxies))
+            )
+
+        if retries <= self.max_retry_times:
+            log.msg(format="Retrying %(request)s (failed %(retries)d times): %(reason)s",
+                    level=log.DEBUG, spider=spider, request=request, retries=retries, reason=reason)
+
+            retryreq = request.copy()
+            retryreq.meta['retry_times'] = retries
+            retryreq.dont_filter = True
+            retryreq.priority = request.priority + self.priority_adjust
+
+            if self.proxies:
+                proxy_address = random.choice(self.proxies.keys())
+                proxy_user_pass = self.proxies[proxy_address]
+                log.msg('Using proxy <%s>, %d proxies left' % (proxy_address, len(self.proxies)))
+                retryreq.meta['proxy'] = proxy_address
+                if proxy_user_pass:
+                    basic_auth = 'Basic ' + base64.encodestring(proxy_user_pass)
+                    retryreq.headers['Proxy-Authorization'] = basic_auth
+
+            return retryreq
+        else:
+            log.msg(format="Gave up retrying %(request)s (failed %(retries)d times): %(reason)s",
+                    level=log.DEBUG, spider=spider, request=request, retries=retries, reason=reason)
--- douappbook/settings.py
+++ douappbook/settings.py
@@ -7,7 +7,11 @@
 #
 #     http://doc.scrapy.org/en/latest/topics/settings.html
 #
+import os
 import MySQLdb
+
+CURR_PATH = os.path.abspath(os.path.dirname(__file__))
+PROJ_PATH = os.path.dirname(CURR_PATH)
 
 BOT_NAME = 'douappbook'
 
@@ -18,9 +22,30 @@ USER_AGENT = 'api-client/0.1.3 com.douban.frodo/2.4.1 iOS/8.2 iPhone6,2'
 
 DEBUG = True
 
+CONCURRENT_ITEMS = 300
+CONCURRENT_REQUESTS = 10
+
 ITEM_PIPELINES = {
     'douappbook.pipelines.DouAppBookPipeline': 300,
 }
+
+# Retry many times since proxies often fail
+RETRY_TIMES = 10
+# Retry on most error codes since proxies fail for different reasons
+RETRY_HTTP_CODES = [500, 503, 504, 403, 404, 408]
+
+DOWNLOADER_MIDDLEWARES = {
+    'scrapy.contrib.downloadermiddleware.retry.RetryMiddleware': None,
+    'douappbook.middlewares.RetryMiddleware': 100,
+    'scrapy.contrib.downloadermiddleware.httpproxy.HttpProxyMiddleware': 110,
+}
+
+# Proxy list containing entries like
+# http://host1:port
+# http://username:password@host2:port
+# http://host3:port
+# ...
+PROXY_LIST = os.path.join(PROJ_PATH, 'proxy.txt')
 
 DB_CONF = {
     'host': 'localhost',
